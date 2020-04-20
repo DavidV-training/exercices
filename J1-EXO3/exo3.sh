0090a7a089ddd043163031b438b778621830612a
bahaliou67@gmail.com <bahaliou67@gmail.com>
2020-04-20 17:12:45 +0200

    Exo3

---
+++ J1-EXO3/exo3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+docker stop mariadb > /dev/null 2>&1
+docker rm mariadb > /dev/null 2>&1
+
+docker stop adminer > /dev/null 2>&1
+docker rm adminer > /dev/null 2>&1
+
+docker run -d \
+  --name mariadb \
+  -e MYSQL_DATABASE=formationdocker \
+  -e MYSQL_USER=david \
+  -e MYSQL_PASSWORD=pictime \
+  -e MYSQL_ROOT_PASSWORD=supermotdepasse \
+  -p 3306:3306 \
+mariadb:10.5
+
+docker run -d \
+  --name adminer \
+  -p 8080:8080 \
+  --link mariadb:db \
+adminer
