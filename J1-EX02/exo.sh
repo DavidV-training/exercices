278cf6be1a6917c1c2e25f9204cf25f1d0f4c90f
Bryan Wallez <bryanwallez.pro@gmail.com>
2020-04-20 15:56:26 +0200

    exo 2 v3

--- J1-EX02/exo.sh
+++ J1-EX02/exo.sh
@@ -3,3 +3,4 @@ mkdir /tmp/nginx-html/ && echo '<html><H1>Exo 2</h1></html>' > /tmp/nginx-html/i
 docker run -d --name nginx -v /tmp/nginx-html:/usr/share/nginx/html:ro -p 8080:80 nginx
 
 docker stop nginx && docker rm nginx
+rm -rf /tmp/nginx-html
