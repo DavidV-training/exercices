59a05516dc8be441e0f72f98de0a29caa3d06945
Romain <rdefente@gmail.com>
2020-04-20 15:14:52 +0200

    correction premier exo

---
+++ J1-EXO/exo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+docker run -d -p 5000:5000 --restart always --name registry registry:2
+
+
+docker pull hello-world
+
+
+docker tag hello-world:latest localhost:5000/rd/hello-world:1.0
+
+
+docker push localhost:5000/rd/hello-world:1.0
