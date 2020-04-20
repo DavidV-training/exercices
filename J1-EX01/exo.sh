641f9e4148c71aa3f29d3c21f0c8150280a627d8
Bryan Wallez <bryanwallez.pro@gmail.com>
2020-04-20 15:12:52 +0200

    exo 1

---
+++ J1-EX01/exo.sh
@@ -0,0 +1,4 @@
+docker run -d -p 5000:5000 --restart always --name registry registry:2
+docker pull hello-world:latest
+docker tag hello-world:latest localhost:5000/dve/hello-world:1.0
+docker push localhost:5000/dve/hello-world:1.0
