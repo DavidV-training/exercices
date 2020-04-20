7229761f57a2f4c1bb24d7bcb87fa5acd817001a
Bryan Wallez <bryanwallez.pro@gmail.com>
2020-04-20 15:23:14 +0200

    nettoyage exo1

--- J1-EX01/exo.sh
+++ J1-EX01/exo.sh
@@ -2,3 +2,7 @@ docker run -d -p 5000:5000 --restart always --name registry registry:2
 docker pull hello-world:latest
 docker tag hello-world:latest localhost:5000/dve/hello-world:1.0
 docker push localhost:5000/dve/hello-world:1.0
+
+docker rmi localhost:5000/dve/hello-world:1.0
+docker rmi hello-world:latest
+docker stop registry
