89558ea0ca02d676fc81a4afc8ba31940b482e98
Bryan Wallez <bryanwallez.pro@gmail.com>
2020-04-20 16:02:44 +0200

    ajout commande bash

--- J1-EX02/exo.sh
+++ J1-EX02/exo.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 mkdir /tmp/nginx-html/ && echo '<html><H1>Exo 2</h1></html>' > /tmp/nginx-html/index.html
 
 docker run -d --name nginx -v /tmp/nginx-html:/usr/share/nginx/html:ro -p 8080:80 nginx
