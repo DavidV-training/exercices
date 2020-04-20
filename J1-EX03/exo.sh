d85458acd61bde0a98589206689b5e11fe2e6154
Bryan Wallez <bryanwallez.pro@gmail.com>
2020-04-20 17:04:29 +0200

    exo3 v2

--- J1-EX03/exo.sh
+++ J1-EX03/exo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker run --name mymariadb -e MYSQL_ROOT_PASSWORD=rooting-password -e MYSQL_USER=imadmin -e MYSQL_DATABASE=mydb -e MYSQL_PASSWORD=yesimadmin -d mariadb:latest
+docker run --name mymariadb -e MYSQL_ROOT_PASSWORD=rooting-password -e MYSQL_USER=imadmin -e MYSQL_DATABASE=mydb -e MYSQL_PASSWORD=yesimadmin -d -p 3306:3306 mariadb:latest
 
-docker run --link mymariadb -p 8080:8080 adminer
+docker run --link mymariadb:db -p 8080:8080 adminer
 
 docker stop mymariadb
 docker stop adminer
