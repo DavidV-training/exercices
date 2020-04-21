9f653afa7cbafb6b1ef49ef3871b5ebc330cbceb
VERGISON David <vergisond@pglw0053.lild01.pictime.fr>
2020-04-21 11:17:51 +0200

    j2 exo2 corrigé

--- J2-EXO2/exo.sh
+++ J2-EXO2/exo.sh
@@ -10,7 +10,9 @@ docker rm nginx > /dev/null 2>&1
 docker run -d \
     --name nginx \
     -p 8080:80 \
-custom-nginx:latest
+    --health-cmd 'if [ "`curl -I -s -o /dev/null -w "%{http_code}" localhost`" = "200" ] ; then exit 0; else exit -1; fi' \
+    --health-interval 5s \
+nginx-dve-multi-ligne
 
 
 #  --health-cmd            Command to run to check health
