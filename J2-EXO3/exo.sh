04859b29fb324b8818da25933f470dcf809f5161
bahaliou67@gmail.com <bahaliou67@gmail.com>
2020-04-21 15:55:56 +0200

    ajout exo J2-EXO3

---
+++ J2-EXO3/exo.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+IMAGE=training/basicservice:mamadou
+CONTAINER=service-java
+OPTIONS="--server.port=8081"
+
+docker stop "${CONTAINER}" > /dev/null 2>&1
+docker rm "${CONTAINER}" > /dev/null 2>&1
+
+echo "==================Build du docker File========================="
+docker build -t ${IMAGE} .
+
+
+docker build -t ${IMAGE} .
+
+docker run -d \
+  --name ${CONTAINER} \
+  -p 8081 \
+  -e MEM_SIZE=256M \
+  --health-interval 1s \
+  --health-cmd 'wget -T 1 -q -O /dev/null localhost:8081/actuator/health' \
+"${IMAGE}" "${OPTIONS}"
+
