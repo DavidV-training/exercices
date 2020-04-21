1ed342b44c110be578749cbf3bb5d552cc6b96b3
VERGISON David <vergisond@pglw0053.lild01.pictime.fr>
2020-04-21 15:24:14 +0200

    j2 exo3

---
+++ J2-EXO3/exo-prod.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+IMAGE=training/basicservice:dve
+CONTAINER=service-java
+
+docker stop ${CONTAINER} > /dev/null 2>&1
+docker rm ${CONTAINER} > /dev/null 2>&1
+
+docker run -d \
+  --name ${CONTAINER} \
+  -P \
+  -e MEM_SIZE=256M \
+"${IMAGE}"
+
