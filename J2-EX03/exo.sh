d5f2502d00717771a862aa38639a20b3ea57f1e3
Bryan Wallez <bryanwallez.pro@gmail.com>
2020-04-22 16:15:17 +0200

    J3 exo2

--- J2-EX03/exo.sh
+++ J2-EX03/exo.sh
@@ -1,3 +1,18 @@
-#!/bin/bash
+IMAGE=training/basicservice:bryan
+CONTAINER=service-java
+OPTIONS='--server.port=8081'
 
-docker build -t training/basicservice:bryan .
+
+docker stop ${CONTAINER} > /dev/null 2>&1
+docker rm ${CONTAINER} > /dev/null 2>&1
+
+
+docker build -t ${IMAGE} .
+
+docker run -d \
+  --name ${CONTAINER} \
+  -p 8081 \
+  -e MEM_SIZE=256M \
+  --health-interval 1s \
+  --health-cmd 'wget -T 1 -q -O /dev/null localhost:8081/actuator/health' \
+"${IMAGE}" "${OPTIONS}"
