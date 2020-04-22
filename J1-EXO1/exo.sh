94381b9b030253c759ad56cedec61244afdc5b80
Herve <herve.nzk@gmail.com>
2020-04-22 12:19:40 +0200

    exercice1

--- J1-EXO1/exo.sh
+++ J1-EXO1/exo.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-docker run -d -p 5000:5000 --restart always --name registry registry:2
-
-
+echo "download registry"
+docker pull registry
+echo "dwownload hello-world"
 docker pull hello-world
-
-docker tag hello-world:latest localhost:5000/dve/hello-word:1.0
-
-
-docker push localhost:5000/dve/hello-word:1.0
+echo "run registry"
+docker run -d -p 5000:5000 --restart always --name registry registry:2
+echo "tag de hello-world"
+docker tag hello-world localhost:5000/hello-world
+echo "push hello-world"
+docker push localhost:5000/hello-world
+echo "Supp de hello world"
+docker rmi hello-world:latest
+echo "Supp de hello world"
+docker rmi localhost:5000/hello-world
