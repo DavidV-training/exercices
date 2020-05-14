f418156b860505648fe7ba5ee09bb9abfc9aa8dd
AlexLefebvre <message.alefe@gmail.com>
2020-05-14 16:49:14 +0200

    Update exo.sh

--- J1-EXO2/exo.sh
+++ J1-EXO2/exo.sh
@@ -5,11 +5,10 @@ docker rm nginx > /dev/null 2>&1
 
 mkdir -p /tmp/exo2/
 
-echo "ok !" > /tmp/exo2/index.htm
+echo "ok !" > /tmp/exo2/index.html
 
-docker run -d \
-    --name nginx \
-    -p 8080:80 \
-    -v /tmp/exo2/:/usr/share/nginx/html:ro \
+docker run -d --name nginx -v /tmp/exo2/:/usr/share/nginx/html:ro -p 8080:80 
+
 nginx:latest
+  
 
