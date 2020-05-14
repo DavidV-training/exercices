983e2bd658e80381096584842cdf36c1d822a870
AlexLefebvre <message.alefe@gmail.com>
2020-05-14 17:14:01 +0200

    Update exo.sh

--- J1-EXO2/exo.sh
+++ J1-EXO2/exo.sh
@@ -5,7 +5,7 @@ docker rm nginx > /dev/null 2>&1
 
 mkdir -p /tmp/exo2/
 
-echo "ok !" > /tmp/exo2/index.html
+echo "bonjour" !" > /tmp/exo2/index.html
 
 docker run -d --name nginx -v /tmp/exo2/:/usr/share/nginx/html:ro -p 8080:80 
 
